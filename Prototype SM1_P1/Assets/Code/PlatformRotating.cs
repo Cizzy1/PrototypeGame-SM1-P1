7284ac5062a2ef3204e95022630ddc7470e61090
Callum Andrews <callumandrews30@gmail.com>
2023-11-17 14:02:05 +0000

    New Input System (Start)

--- Prototype SM1_P1/Assets/Code/PlatformRotating.cs
+++ Prototype SM1_P1/Assets/Code/PlatformRotating.cs
@@ -2,36 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlatformRotating : MonoBehaviour
 {
-    public float RotateSpeed;
+    //public float RotateSpeed;
+    Rigidbody rb;
+    public float Torque;
+
+    PlayerControls controls;
+
+    void Awake(){
+        controls = new PlayerControls();
+
+        controls.Gameplay.Movement.performed += ctx => rotate();
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {   
         if(Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.right * RotateSpeed * Time.deltaTime);
+            //transform.Rotate(Vector3.right * RotateSpeed * Time.deltaTime);
+            rb.AddTorque(transform.right * Torque);
         }
 
         if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(Vector3.left * RotateSpeed * Time.deltaTime);
+            //transform.Rotate(Vector3.left * RotateSpeed * Time.deltaTime);
+            rb.AddTorque(-transform.right * Torque);
         }
 
         if(Input.GetKey(KeyCode.W)){
-            transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
+            //transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
+            rb.AddTorque(transform.forward * Torque);
         }
 
         if(Input.GetKey(KeyCode.S)){
-            transform.Rotate(Vector3.back * RotateSpeed * Time.deltaTime);
-
+            //transform.Rotate(Vector3.back * RotateSpeed * Time.deltaTime);
+            rb.AddTorque(-transform.forward * Torque);
            
         }
-    }       
+    }
+
+    void rotate(){
+        rb.AddTorque(transform.forward * Torque)''
+    }
+
+    void OnEnable(){
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable(){
+        controls.Gameplay.Disable();
+    }
+
+    
 }
